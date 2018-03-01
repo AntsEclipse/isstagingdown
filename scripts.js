c51e6aea64922b65c9d2ded51e59016d230ff77c
Anthony <anthony3h18@gmail.com>
2018-03-01 15:19:11 -0500

    add setInterval check

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
-window.onload = () => {
+let timeout = 30000; // 30 second
+
+const getStatus = () => {
   fetch('https://api-staging.compass-srp.com/auth',  {
     method: 'OPTION'
   }).then(response => {
     if (response.ok) {
+      timeout = 120000; // 2 minute
       document.getElementById('status').textContent = 'No'
     } else {
+      timeout = 30000;
       document.getElementById('status').textContent = 'Staging is Down'
     }
   }).catch(() => {
+    timeout = 30000;
     document.getElementById('status').textContent = 'Staging is Down';
-  });
+  })
 };
+
+
+window.onload = getStatus;
+window.setInterval(getStatus, timeout);
